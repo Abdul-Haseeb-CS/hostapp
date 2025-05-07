2c6fa0db4dd23950c03df5c83d685a29a0b5e779
Abdul Haseeb <v.abdulhaseeb@gmail.com>
2025-05-07 10:40:50 +0500

    tailwind issue

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,11 +1,20 @@
 import logo from './logo.svg';
+import './index.css';
 import './App.css';
-
+import  Navbar from './components/homepage/navbar';
+import  Homepage from './components/homepage/homepage';
 function App() {
   return (
+    <>
+    <Homepage/>
+    
+    <Navbar/>
+
     <h1 className="text-3xl font-bold underline">
     Hello world!
   </h1>
+
+  </>
   );
 }
 
